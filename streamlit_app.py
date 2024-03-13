73c7d6db75ae980ac6687d816ffdf29d99740dd1
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-03-13 22:00:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,36 @@
 import streamlit as st
+import google.generativeai as genai
+import random
 
-# Define a dictionary of manhwas with genres
-manhwas = {
-    'Action': ['Solo Leveling', 'Tower of God', 'The Breaker'],
-    'Romance': ['True Beauty', 'The Secret of Angel', 'Love Alarm'],
-    'Fantasy': ['Noblesse', 'Black Haze', 'The Gamer']
-}
+genai.configure(api_key='YOUR_API_KEY')  # Replace 'YOUR_API_KEY' with your actual API key
 
-# Define function to recommend manhwas based on selected genres
-def recommend_manhwas(selected_genres):
-    recommended_manhwas = []
-    for genre in selected_genres:
-        recommended_manhwas.extend(manhwas.get(genre, []))
-    return recommended_manhwas
-
-# Streamlit UI
-st.title('Manhwa Recommendation App')
-
-# Genre selection
-selected_genres = st.multiselect('Select genres', list(manhwas.keys()))
-
-if selected_genres:
-    # Recommend manhwas
-    recommended_manhwas = recommend_manhwas(selected_genres)
+def prompt(manhwa_title):
+    prompt_parts = [
+        f"'As a avid manhwa reader with a wide range of preferences,"
+        f"recommend five manhwas similar to the given title: {manhwa_title},"
+        f"Also for each manhwa, explain why you find it similar to the given title and add --- before recommending the next manhwa'",
+    ]
+    return prompt_parts
     
-    # Display recommendations
-    st.subheader('Recommended Manhwas:')
-    for manhwa in recommended_manhwas:
-        st.write('- ' + manhwa)
-else:
-    st.write('Please select at least one genre.')
+def recommend(manhwa_title, model):
+    human_prompt = prompt(manhwa_title)
+    response = model.generate_content(human_prompt)
+    return response.text
 
+def main():
+    st.title('Manhwa Recommendation App')
+    manhwa_title = st.text_input('Enter the manhwa title:')
+    if manhwa_title:
+        model = genai.GenerativeModel(model_name="gemini-pro")
+        response = recommend(manhwa_title, model)
+        recommendations = response.split('---') 
+        st.write("Recommendations and Why you'd like them")
+        for i, recommendation in enumerate(recommendations, start=1):
+            text_color = '#000000'
+            background_color = '#' + '%06x' % random.randint(0, 0xFFFFFF) 
+            style = f"color: {text_color}; background-color: {background_color}; padding: 10px; border-radius: 15px; margin-bottom: 45px;"
+            recommendation_text = recommendation.replace('**', '<strong>', 1).replace('**', '</strong>', 1)
+            st.markdown(f"<div style='{style}'>{recommendation_text}</div>", unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    main()
