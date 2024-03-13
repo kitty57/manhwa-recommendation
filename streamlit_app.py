29c36145a5d1f60f9c6d1b5acabb6c65c50d5462
Sanjana <97579455+kitty57@users.noreply.github.com>
2024-03-13 21:51:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,33 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+# Define a dictionary of manhwas with genres
+manhwas = {
+    'Action': ['Solo Leveling', 'Tower of God', 'The Breaker'],
+    'Romance': ['True Beauty', 'The Secret of Angel', 'Love Alarm'],
+    'Fantasy': ['Noblesse', 'Black Haze', 'The Gamer']
+}
 
-st.write('Hello world!')
+# Define function to recommend manhwas based on selected genres
+def recommend_manhwas(selected_genres):
+    recommended_manhwas = []
+    for genre in selected_genres:
+        recommended_manhwas.extend(manhwas.get(genore, []))
+    return recommended_manhwas
+
+# Streamlit UI
+st.title('Manhwa Recommendation App')
+
+# Genre selection
+selected_genres = st.multiselect('Select genres', list(manhwas.keys()))
+
+if selected_genres:
+    # Recommend manhwas
+    recommended_manhwas = recommend_manhwas(selected_genres)
+    
+    # Display recommendations
+    st.subheader('Recommended Manhwas:')
+    for manhwa in recommended_manhwas:
+        st.write('- ' + manhwa)
+else:
+    st.write('Please select at least one genre.')
+
